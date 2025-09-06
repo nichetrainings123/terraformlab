a5a29745ba045df0d1d14f36af2f9ba4a8066eac
nichetrainings123 <66190130+nichetrainings123@users.noreply.github.com>
2025-09-06 14:48:01 +0530

    Update main.tf

--- lab-module/main.tf
+++ lab-module/main.tf
@@ -1,1 +1,18 @@
+module "vpc" {
+  source      = "./modules/vpc"
+  vpc_cidr    = "10.0.0.0/16"
+  subnet_cidr = "10.0.1.0/24"
+}
+
+module "ec2" {
+  source        = "./modules/ec2"
+  instance_type = "t2.micro"
+  ami_id        = "ami-0a0b0b06dd1636865" # Amazon Linux 2
+  subnet_id     = module.vpc.subnet_id
+}
+
+module "s3" {
+  source      = "./modules/s3"
+  bucket_name = "my-terraform-bucket-841226"
+}
 
