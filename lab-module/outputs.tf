954989d09cb8e1d652aa1485a01f8f8fe55d9c40
nichetrainings123 <66190130+nichetrainings123@users.noreply.github.com>
2025-09-06 14:48:21 +0530

    Update outputs.tf

--- lab-module/outputs.tf
+++ lab-module/outputs.tf
@@ -1,1 +1,12 @@
+output "ec2_instance_id" {
+  value = module.ec2.instance_id
+}
+
+output "ec2_public_ip" {
+  value = module.ec2.public_ip
+}
+
+output "s3_bucket_name" {
+  value = module.s3.bucket_name
+}
 
